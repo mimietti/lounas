638815dd9fcf89753fb9c62585402e6655fb5aa0
Michael Miettinen <michael.miettinen@iki.fi>
2026-05-13 16:05:41 +0300

    Refactor restaurant display to use card elements
    Updated the restaurant card creation to include links and changed the element from 'div' to 'card'.

--- app.js
+++ app.js
@@ -1,19 +1,35 @@
 const restaurants = [
   {
+    name: "Ravintola Lokki",
+    url: "https://ravintolalokki.fi/lounas-lahdessa/"
+  },
+  {
     name: "Sibeliustalo",
     url: "https://www.sibeliustalo.fi/ravintolat/viikon-lounaslista/"
+  },
+  {
+    name: "Einstein",
+    url: "https://lounasravintolaeinstein.com/lounas/"
+  },
+  {
+    name: "KU Center M19",
+    url: "https://kucenter.fi/m19-food-market/"
   }
 ]
 
 const container = document.getElementById("menus")
 
 restaurants.forEach(r => {
-  const div = document.createElement("div")
+  const card = document.createElement("div")
 
-  div.innerHTML = `
+  card.className = "card"
+
+  card.innerHTML = `
     <h2>${r.name}</h2>
-    <iframe src="${r.url}" width="100%" height="600"></iframe>
+    <a href="${r.url}" target="_blank">
+      Avaa lounaslista
+    </a>
   `
 
-  container.appendChild(div)
+  container.appendChild(card)
 })
